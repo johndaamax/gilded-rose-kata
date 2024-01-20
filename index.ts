1332356dd786eb3b9d92c406f61a4b3b8527644a
johndaamax <ia.uoistud@gmail.com>
2024-01-20 13:50:53 +0100

    add implementation of Aged Brie

--- index.ts
+++ index.ts
@@ -15,6 +15,11 @@ const ITEM_VAULT = new Map<string, (item: Item) => void>();
 
 // Sulfuras never needs to update quality or sell date
 ITEM_VAULT.set("Sulfuras, Hand of Ragnaros", (_) => {});
+ITEM_VAULT.set("Aged Brie", (item) => {
+  // increase quality of Aged Brie until it reaches the limit
+  item.quality = item.quality + 1 > 50 ? 50 : item.quality + 1;
+  item.sellIn -= 1;
+});
 
 class Shop {
   public items: Item[];
