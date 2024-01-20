7419b38ae936524821d3b1e39b460b2d73bb8783
johndaamax <ia.uoistud@gmail.com>
2024-01-20 14:09:16 +0100

    add test cases for Backstage passes

--- index.test.ts
+++ index.test.ts
@@ -25,4 +25,39 @@ describe("Gilded Rose", function () {
       expect(items[0].sellIn).toBe(24);
     });
   });
+
+  describe("Backstage passes", () => {
+    it("should remove all quality when its sell day has passed", function () {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", -1, 25),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(0);
+      expect(items[0].sellIn).toBe(-2);
+    });
+    it("should increase quality by 2 when its sell day is <=10", function () {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 7, 25),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(27);
+      expect(items[0].sellIn).toBe(6);
+    });
+    it("should increase quality by 3 when its sell day is <=5", function () {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 2, 35),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(38);
+      expect(items[0].sellIn).toBe(1);
+    });
+    it("should increase the Backstage Pass quality by 1 when its sell day > 10", function () {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 12, 19),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(20);
+      expect(items[0].sellIn).toBe(11);
+    });
+  });
 });
