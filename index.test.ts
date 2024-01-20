e65e2ff50a3da67bcab7ef0da3a90b1de74554f0
johndaamax <ia.uoistud@gmail.com>
2024-01-20 13:51:42 +0100

    add test case of max quality Aged Brie

--- index.test.ts
+++ index.test.ts
@@ -11,10 +11,18 @@ describe("Gilded Rose", function () {
     expect(items[0].sellIn).toBe(10);
   });
 
-  it("should increase Aged Brie quality by 1", function () {
-    const gildedRose = new Shop([new Item("Aged Brie", 10, 12)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].quality).toBe(13);
-    expect(items[0].sellIn).toBe(9);
+  describe("Aged Brie", () => {
+    it("should increase quality by 1 if < 50", function () {
+      const gildedRose = new Shop([new Item("Aged Brie", 10, 12)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(13);
+      expect(items[0].sellIn).toBe(9);
+    });
+    it("should cap quality at 50", function () {
+      const gildedRose = new Shop([new Item("Aged Brie", 25, 50)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].quality).toBe(50);
+      expect(items[0].sellIn).toBe(24);
+    });
   });
 });
