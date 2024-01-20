39b7797cbbd2e3e21ab02cc7ee2c086fbf04ac83
johndaamax <ia.uoistud@gmail.com>
2024-01-20 14:54:44 +0100

    move item vault map into its own separate file

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { getNextQuality } from "./utils";
+import { ITEM_VAULT } from "./vault";
 
 class Item {
   public name: string;
@@ -13,42 +13,6 @@ class Item {
 }
 // the base value of how much the quality will degrade each day
 const BASE_DEGRADING_QUALITY = 1;
-// create a vault of items that have the item name as keys and a function to update quality and sell date as values
-const ITEM_VAULT = new Map<
-  string,
-  (item: Item, degradingQuality: number) => void
->();
-
-// Sulfuras never needs to update quality or sell date
-ITEM_VAULT.set("Sulfuras, Hand of Ragnaros", (_) => {});
-ITEM_VAULT.set("Aged Brie", (item) => {
-  // increase quality of Aged Brie until it reaches the limit
-  item.quality = getNextQuality(item.quality + 1);
-  item.sellIn -= 1;
-});
-ITEM_VAULT.set("Backstage passes to a TAFKAL80ETC concert", (item) => {
-  // increase quality of backstage passes according to the requirements
-  if (item.sellIn < 0) {
-    item.quality = 0;
-  } else if (item.sellIn <= 5) {
-    item.quality = getNextQuality(item.quality + 3);
-  } else if (item.sellIn <= 10) {
-    item.quality = getNextQuality(item.quality + 2);
-  } else {
-    item.quality = getNextQuality(item.quality + 1);
-  }
-  item.sellIn -= 1;
-});
-ITEM_VAULT.set("Conjured Mana Cake", (item: Item, degradingQuality: number) => {
-  // lower the quality of Conjured items twice as much
-  const CONJURED_DEGRADING_QUALITY = 2 * degradingQuality;
-  item.quality = getNextQuality(item.quality - CONJURED_DEGRADING_QUALITY);
-  item.sellIn -= 1;
-});
-ITEM_VAULT.set("***", (item: Item, degradingQuality: number) => {
-  item.quality = getNextQuality(item.quality - degradingQuality);
-  item.sellIn -= 1;
-});
 
 class Shop {
   public items: Item[];
