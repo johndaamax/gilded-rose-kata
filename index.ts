64de490b8ea5ede8b4d646c57a925ca44fcbdbe2
johndaamax <ia.uoistud@gmail.com>
2024-01-20 13:41:06 +0100

    add base item vault structure and include Sulfuras

--- index.ts
+++ index.ts
@@ -10,12 +10,22 @@ class Item {
   }
 }
 
+// create a vault of items that have the item name as keys and a function to update quality and sell date as values
+const ITEM_VAULT = new Map<string, (item: Item) => void>();
+
+// Sulfuras never needs to update quality or sell date
+ITEM_VAULT.set("Sulfuras, Hand of Ragnaros", (_) => {});
+
 class Shop {
   public items: Item[];
   constructor(items: Item[] = []) {
     this.items = items;
   }
   updateQuality() {
+    this.items.forEach((item) => {
+      const qualityHandler = ITEM_VAULT.get(item.name);
+      qualityHandler?.(item);
+    });
     return this.items;
   }
 }
